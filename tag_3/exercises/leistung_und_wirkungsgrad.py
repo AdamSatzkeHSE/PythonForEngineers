dbfab0c60b253fc15380f66d671fd08faca340fc
AdamSatzkeHSE <adam.satzke@hamsterrad-se.de>
2026-03-18 08:46:46 -0300

    update

--- tag_3/exercises/leistung_und_wirkungsgrad.py
+++ tag_3/exercises/leistung_und_wirkungsgrad.py
@@ -1,14 +1,11 @@
 # Ausgangssituation:
 # Ein Gleichstrommotor wird bei verschiedenen Betriebspunkten untersucht.
 # Es liegen Eingangsspannung, Eingangsstrom und abgegebene mechanische Leistung vor.
-# Ziel ist es, elektrische Leistung und Wirkungsgrad zu berechnen und grafisch darzustellen.
+# Ziel ist es, elektrische Leistung und Wirkungsgrad zu berechnen und 
+# grafisch darzustellen.
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# Hallo das ist ein Kommentar
-
 
 # Messdaten des Motors.
 voltage_V = np.array([12, 12, 12, 12, 12], dtype=float)
@@ -16,7 +13,6 @@ current_A = np.array([0.8, 1.2, 1.8, 2.4, 3.0], dtype=float)
 
 # Aufgabe 1:
 # Berechne die elektrische Eingangsleistung P_el = U * I.
-
 
 # Aufgabe 2:
 # Berechne den Wirkungsgrad eta = P_mech / P_el.
@@ -26,5 +22,7 @@ current_A = np.array([0.8, 1.2, 1.8, 2.4, 3.0], dtype=float)
 
 # Aufgabe 4:
 # Stelle den Wirkungsgrad über dem Strom dar.
+
+# Extra / Optional: annotation verwenden, um in der Grafik den Wirkungsgrad zu zeigen.
 
 fig = plt.figure()

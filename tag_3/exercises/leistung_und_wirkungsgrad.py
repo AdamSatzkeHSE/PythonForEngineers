57dc453c8c4958ddad175729efcb44b424bacc74
AdamSatzkeHSE <adam.satzke@hamsterrad-se.de>
2026-03-18 08:48:14 -0300

    update

--- tag_3/exercises/leistung_und_wirkungsgrad.py
+++ tag_3/exercises/leistung_und_wirkungsgrad.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 # Messdaten des Motors.
 voltage_V = np.array([12, 12, 12, 12, 12], dtype=float)
 current_A = np.array([0.8, 1.2, 1.8, 2.4, 3.0], dtype=float)
+mechanical_power_W = np.array([6.5, 10.0, 14.8, 18.5, 21.0], dtype=float)
 
 # Aufgabe 1:
 # Berechne die elektrische Eingangsleistung P_el = U * I.

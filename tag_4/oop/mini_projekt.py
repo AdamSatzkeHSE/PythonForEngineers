e78e176729a450608299fff0967e873074835d13
AdamSatzkeHSE <adam.satzke@hamsterrad-se.de>
2026-04-15 09:55:30 -0300

    mini-projekt oop

---
+++ tag_4/oop/mini_projekt.py
@@ -0,0 +1,28 @@
+"""Mini-Projekt
+Ihr sollt selbst eine Klasse bauen, die:
+- Daten lädt
+- Leistung berechnet
+- Mittelwerte ausgibt
+- einen Plot erstellt
+- optional eine Animation zeigt
+
+Messdaten eines elektrischen Systems sollen strukturiert verarbeitet werden.
+Die Daten sollen geladen, analysiert und visualisiert werden.
+Dafür wird eine Klasse verwendet, die Daten und zugehörige Methoden zusammenfasst.
+"""
+
+class MeasurementAnalyzer:
+    def __init__(self, filename):
+        pass
+
+    def compute_power(self):
+        pass
+
+    def animate_voltage(self):
+        # def update(frame) ...
+        pass
+
+
+
+# Test
+analyzer = MeasurementAnalyzer("measurement.csv")

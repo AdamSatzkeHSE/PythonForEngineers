57385843b7d22dc83fe62025771272c9e47b87ac
AdamSatzkeHSE <adam.satzke@hamsterrad-se.de>
2026-04-15 06:18:13 -0300

    aufgabe 2

--- tag_4/matlab/aufgabe_1.py
+++ tag_4/matlab/aufgabe_1.py
@@ -6,3 +6,47 @@ Aufgabe:
 1. Erzeuge ein Sinussignal
 2. Stelle es animiert dar (wachsender Plot)
 """
+
+# 1. Module importieren
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Anfangsbedingungen
+t = np.linspace(0, 10, 100)
+signal = np.sin(t)
+
+# Figur und Achse erstellen
+fig, ax = plt.subplots()
+
+# Leere Linie erstellen
+print(type(ax.plot([], [])))
+print(ax.plot([], []))
+# line = ax.plot([], [])[0]
+# line_tupel = tuple(line)
+line, = ax.plot([], [])
+
+# Achsenbegrenzung setzen
+ax.set_xlim(0, 10)
+ax.set_ylim(-1.5, 1.5)
+
+# Update-Funktion wird für jeden Frame aufgerufen
+def update(frame):
+    # Daten bis zum aktuellen Frame anzeigen
+    x = t[:frame]
+    y = signal[:frame]
+
+    # Linie aktualisieren
+    line.set_data(x, y)
+
+    return line,
+
+# FuncAnimation steuert die Animation
+ani = FuncAnimation(
+    fig,
+    update,
+    frames=len(t),
+    interval=50
+)
+
+plt.show()
---
+++ tag_4/matlab/aufgabe_2.py
@@ -0,0 +1,15 @@
+"""
+Ausgangssituation:
+Ein Kondensator entlädt sich über die Zeit.
+
+Formel:
+U(t) = U0 * exp(-t / tau)
+
+Aufgabe:
+1. Simuliere die Entladung
+2. Stelle sie animiert dar
+
+Erweitert:
+Füge Rauschen hinzu
+Stelle das verrauschte Signal animiert im selben Plot dar
+"""

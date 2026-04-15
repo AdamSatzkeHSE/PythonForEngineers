4a5920b2347a7d5a69215b68cf11caf3e29c234d
AdamSatzkeHSE <adam.satzke@hamsterrad-se.de>
2026-04-15 05:49:05 -0300

    Aufgaben

---
+++ tag_4/matlab/aufgabe_1.py
@@ -0,0 +1,8 @@
+"""
+Ein Sensorsignal soll nicht sofort komplett angezeigt werden,
+sondern sich Schritt für Schritt über die Zeit aufbauen.
+
+Aufgabe:
+1. Erzeuge ein Sinussignal
+2. Stelle es animiert dar (wachsender Plot)
+"""
